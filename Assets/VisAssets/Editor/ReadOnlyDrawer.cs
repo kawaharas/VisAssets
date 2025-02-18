1e3705b5218ea6443263a8e204a5870cd14647f2
Shintaro Kawahara <kawahara@jamstec.go.jp>
2025-02-18 22:47:40 +0900

    fixed ReadOnlyAttribute

--- Assets/VisAssets/Editor/ReadOnlyDrawer.cs
+++ Assets/VisAssets/Editor/ReadOnlyDrawer.cs
@@ -1,11 +1,6 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-public class ReadOnlyAttribute : PropertyAttribute
-{
-}
-
-
 [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 public class ReadOnlyDrawer : PropertyDrawer
 {
--- Assets/VisAssets/Scripts/ReadOnlyAttribute.cs
+++ Assets/VisAssets/Scripts/ReadOnlyAttribute.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEngine;
 
-/*
 public class ReadOnlyAttribute : PropertyAttribute
 {
 }
-*/
